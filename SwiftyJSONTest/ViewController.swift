4b17f3a34db175b1c130a95e450677c7077c9cfe
Samieh <samieh.sadeghi@iths.se>
2020-02-12 15:06:05 +0100

    Initial Commit

---
+++ SwiftyJSONTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyJSONTest
+//
+//  Created by Samieh Sadeghi on 2020-02-12.
+//  Copyright © 2020 Samieh Sadeghi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
